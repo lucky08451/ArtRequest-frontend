a0604ec1191e6da1a83da09c6505c704ed6e3eb4
LOBOBO <qfkuch08451@gmail.com>
2025-03-10 13:23:50 +0800

    新增home頁面

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Layout from '@/views/Layout/index.vue'
+import Home from '@/views/Home/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'layout',
       component: Layout,
+      children: [
+        {
+          path: "",
+          name: "home",
+          component: Home,
+        }
+      ],
     },
     {
       path: '/about',
---
+++ src/views/Home/index.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <div class="md-8">
+          <h1>歡迎來到 Art Request</h1>
+          <p>這是一個線上藝術委託平台</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
